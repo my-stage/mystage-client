82de7805b3f6af8b4c820ad6583c6bca7368bcf3
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2019-11-24 17:55:47 +0100

    add first demo for audio player

--- src/pages/AudioPlayer.tsx
+++ src/pages/AudioPlayer.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Typography
+} from '@material-ui/core';
+import WaveSurfer from 'wavesurfer.js';
 
 function AudioPlayer() {
-  return <div>Audio Player</div>;
+  const [waveSurfer, setWaveSurfer] = useState();
+
+  useEffect(() => {
+    const waveSurfer = WaveSurfer.create({
+      container: '#waveform'
+    });
+    waveSurfer.load('data/Tina Turner.mp3');
+    waveSurfer.on('ready', function () {
+      waveSurfer.play();
+    });
+    setWaveSurfer(waveSurfer);
+  }, []);
+
+  return (
+    <>
+      <Typography>Audio Player</Typography>
+      <div id="waveform"></div>
+    </>
+  );
 }
 
 export default AudioPlayer;
