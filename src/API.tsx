e58ee763a905cabad66969ab6b46ea1bc1de8c31
Niklas Gschaider <niklas.gschaider@gschaider-systems.at>
2019-11-24 19:07:13 +0000

    added basic api + example

---
+++ src/API.tsx
@@ -0,0 +1,36 @@
+var apiUrl = "https://mystage.ngschaider.at/api/v1/";
+
+async function otherRequest(method: string, data: object) {
+  const response = await fetch(apiUrl, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+async function getRequest(urlText: string, params?: object){
+  var url = new URL(urlText);
+  if(params) {
+    //Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  }
+  const response = await fetch(url.toString());
+  return await response.json();
+}
+async function request(method: string, url: string, dataOrParams={}) {
+  if(method === "GET") {
+    return getRequest(url, dataOrParams);
+  } else {
+    return otherRequest(method, dataOrParams);
+  }
+}
+
+export default class API {
+
+  static async getUsers() {
+    const response = await request("GET", apiUrl + "records/users");
+    return response.records;
+  }
+
+}
--- src/pages/UsersMgmt.tsx
+++ src/pages/UsersMgmt.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../API';
 
 function UsersMgmt() {
-  return <div>Users Mgmt</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    API.getUsers().then(setUsers);
+  }, []);
+
+  return (
+    <>
+      <h1>Users Management</h1>
+      {users.map(user => (
+        <>
+          <h2>{user["username"]}</h2>
+          <h2>{user["name"]}</h2>
+        </>
+      ))}
+    </>
+  );
 }
 
 export default UsersMgmt;
